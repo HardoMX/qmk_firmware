8481092ede4b880d02ccbe69f0e4c75663fc3f39
HardoMX <hardo314@gmail.com>
2024-02-10 19:10:12 +0100

    ÃÃadd pipe symbol

--- keyboards/rgbkb/sol3/keymaps/HardoMX/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/HardoMX/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SOUL] = LAYOUT(
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_NO,                 KC_BSLS,  KC_6,   KC_7,    KC_8,    KC_9,    KC_0,   ADJUST,
-        KC_TAB,  KC_Q,    KC_W,    KC_L,    KC_D,  KC_P,   KC_NO,                 KC_NO, KC_K,   KC_M,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
+        KC_TAB,  KC_Q,    KC_W,    KC_L,    KC_D,  KC_P,   KC_NO,                 KC_PIPE, KC_K,   KC_M,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
         FN_CWTG, KC_A,    KC_S,    KC_R,    KC_T,  KC_G,   KC_NO,                 BRCKTS,   KC_F,   KC_N,    KC_E,    KC_I,    KC_O,   KC_MINS,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_J,   KC_NO,                 CURLS,   KC_B,   KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRAVE,
         KC_LCTL, KC_LGUI, KC_NO, ADJUST, KC_LALT, KC_SPC, KC_NO, KC_DEL, KC_ENT, KC_NO, KC_BSPC, AO, AE, OE, KC_EQL, KC_RCTL,
